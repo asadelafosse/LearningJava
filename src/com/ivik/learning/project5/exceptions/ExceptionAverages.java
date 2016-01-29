8b4b7d8f559b9cc13e87cf35c06a20963eca45c4
Sebastien <asadelafosse@hotmail.com>
2016-01-29 02:12:53 +0100

    Project 5

---
+++ src/com/ivik/learning/project5/exceptions/ExceptionAverages.java
@@ -0,0 +1,40 @@
+package com.ivik.learning.project5.exceptions;
+
+/**
+ * Created by Sebastien on 29-1-2016.
+ */
+public class ExceptionAverages {
+
+    private double averageScore(double totalScore, String numberStudentsAsString){
+
+        try {
+            int numberStudents = Integer.parseInt(numberStudentsAsString);
+            int averageScore = (int) totalScore / numberStudents;       // recasting to int is required to receive a divide by zero arithmetic exception.
+            System.out.println("Average score for (" + totalScore + ", " + numberStudentsAsString + ") is: " + averageScore);
+            return averageScore;
+
+        } catch(NumberFormatException e1){
+            System.out.println("Average score for (" + totalScore + ", " + numberStudentsAsString + ") resulted in a NumberFormatException.");
+
+        } catch(ArithmeticException e2){
+            System.out.println("Average score for (" + totalScore + ", " + numberStudentsAsString + ") resulted in an ArithmeticException.");
+
+        } finally {return 1;}       //The value returned here is not relevant. If the try block works, averageScore is return, and otherwise, the returned value is not used.
+
+    }
+
+    public void testAverages(){
+        averageScore(900, "10");
+        averageScore(90,"Oops!");
+        averageScore(90,"0");
+    }
+
+    public static void main(String[] args) {
+        ExceptionAverages exceptionAverages = new ExceptionAverages();
+        exceptionAverages.testAverages();
+
+
+
+    }
+
+}
